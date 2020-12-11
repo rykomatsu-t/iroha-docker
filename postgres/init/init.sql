7e3426af55ef4fad9637f8fa6fe848d841443367
rykomtsu-t <62225404+rykomtsu-t@users.noreply.github.com>
2020-12-11 17:48:35 +0900

    アプリ用のDBを追加

--- postgres/init/init.sql
+++ postgres/init/init.sql
@@ -1,1 +1,13 @@
-CREATE DATABASE pgtest;
+set client_encoding = 'UTF8';
+
+create table users (
+  id serial primary key,
+  name varchar not null,
+  password varchar not null
+);
+
+insert into users(name, password) values 
+  ('yamada', 'password'),
+  ('tanaka', 'password'),
+  ('satou', 'password')
+;
